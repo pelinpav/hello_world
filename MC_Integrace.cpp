88001f6b69296253df3fb6b30395e06a7f600f4f
pelinpav <91843090+pelinpav@users.noreply.github.com>
2021-11-08 23:24:21 +0100

    Add files via upload

---
+++ MC_Integrace.cpp
@@ -0,0 +1,30 @@
+#include "MC_Integrace.h"
+#include <fstream>
+using namespace std;
+// V inicializacnim seznamu se provede inicializace nahodne distribuce
+MC_Integrace::MC_Integrace(int _n, double a, double b) :n(_n), distribution(a,b),vystupni_soubor("tvar.txt")
+{
+    vol = (b-a)*(b-a);
+}
+//fstream vystupni_soubor ("tvar.txt", ios::out);
+double MC_Integrace::vypocet(Shape const& s) {
+    int n_trefy = 0;
+
+   // fstream uloz_data ("hovno.txt", ios::out);
+    for (int i=0;i<n;++i) {
+        Point shot = Point(distribution(generator),distribution(generator));
+        if (s.is_inside(shot))
+
+         if (n_trefy %100==0) {
+         vystupni_soubor<<shot.x<<" "<<shot.y;
+         vystupni_soubor<<"\n" ;
+         }     // uloz_data <<shot.x<<" "<<shot.y;
+//uloz_data<<"\n";
+
+n_trefy++;
+
+
+    }
+    return vol*double(n_trefy)/double(n);
+
+}
--- main.cpp
+++ main.cpp
@@ -1,46 +1,36 @@
-#include<iostream>
-#include<cstdio>
-#include<cmath>
-#include<fstream>
-#include<tuple>
-#include<vector>
-#include "clovek.h"
-using namespace std;
+#include <iostream>
+#include <memory>
+#include "Shape.h"
+#include "MC_Integrace.h"
 
 int main()
 {
-    vector<Clovek> seznam_osob;
-    string dalsi_osoba = "y";
+    // Polymorfni ukazatel, muzeme do nej ukladat libovolne objekty
+    // jakekoli tridy odvozene od Shape
+    std::shared_ptr<Shape> s;
 
-    do {
-    Clovek osoba;
+    int utvar;
+    std::cout << "Vyberte druh utvaru (1 - kruh, 2 - kyticka): " << std::endl;
+    std::cin >> utvar;
 
-    string jmeno, prijmeni;
-    tie(jmeno, prijmeni) = nacti_jmeno();
-    cout << "Cele jmeno uzivatele: " << jmeno << " " << prijmeni << endl;
-    osoba.jmeno = jmeno;
-    osoba.prijmeni = prijmeni;
+    // Podle uzivatelskeho zadani pripravime vybrany objekt a nasmerujeme na nej ukazatel
+    // Parametry funkce make_shared jsou stejne jako parametry konstruktoru naseho objektu
+    if (utvar == 1) {
+        s = std::make_shared<Circle>(Point(0.0,0.0),1.0);
+    }
+    else if (utvar == 2) {
+        s = std::make_shared<Flower>(Point(0.0,0.0),1.0,4.0);
+    }
 
-    int vek = nacti_vek(0, 150);
-    cout << "Vek uzivatele: " << vek << endl;
-    osoba.vek = vek;
+    // Nyni vypocitame integral metodou Monte Carlo
+    const int n_shots = 10000;
+    MC_Integrace integral(n_shots,-1.0,1.0);
+    double obsah = integral.vypocet(*s); // Pri predavani objektu musime ukazatel dereferencovat operatorem *
+                                         // Jinak by se prekladac snazil predat funkci pouze
+                                         // ciselnou hodnotu adresy v pameti
 
-    float vyska = nacti_vysku(0, 250);
-    cout << "Vyska uzivatele: " << vyska << endl;
-    osoba.vyska = vyska;
-
-    seznam_osob.push_back(osoba);
-    uloz_do_souboru(jmeno, prijmeni, vek, vyska);
-
-    cout << "Pridat dalsi osobu? (Y/N)" << endl;
-    cin >> dalsi_osoba;
-    } while(dalsi_osoba == "y" || dalsi_osoba == "Y");
-
-    float prumerny_vek = urcit_prumerny_vek(seznam_osob);
-    cout << "Prumerny vek osob: " << prumerny_vek << endl;
-
-    float prumerna_vyska = urcit_prumerna_vyska(seznam_osob);
-    cout << "Prumerna vyska osob: " << prumerna_vyska << endl;
+    std::cout << "Provedeno "<< n_shots << " iteraci MC metody." << std::endl;
+    std::cout << "Obsah utvaru je " << obsah << std::endl;
 
     return 0;
 }
